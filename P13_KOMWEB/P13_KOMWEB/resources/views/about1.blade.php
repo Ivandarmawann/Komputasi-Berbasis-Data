ae56ca1bea0f264061a7b1dee845ac2293b5acd1
Muhammad Ivan Darmawan <mhmmdipand@gmail.com>
2025-01-03 14:14:15 +0700

    Delete P13_KOMWEB/P13_KOMWEB directory

--- P13_KOMWEB/P13_KOMWEB/resources/views/about1.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'About')
-
-@section('content')
-    <p>
-        <strong>Ini adalah halaman About</strong>
-    </p>
-@endsection
--- P13_KOMWEB/P13_KOMWEB/resources/views/contact1.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'Contact')
-
-@section('content')
-    <p>
-        <strong>Ini adalah halaman Contact</strong>
-    </p>
-@endsection
